c91dbaeb1d8dc41601f9f98a5cf66abe46443a27
MitchS134 <49328485+MitchS134@users.noreply.github.com>
2020-10-23 23:16:24 -0400

    More Animations and Tiles

--- Godot Project/Enemy_1.gd
+++ Godot Project/Enemy_1.gd
@@ -15,34 +15,34 @@ var next_dir = 0
 var next_dir_time = 0
 
 #func _ready():
-    #$CollisionShape2D.disabled = true
+	#$CollisionShape2D.disabled = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 
-    if Player.position.x < position.x and next_dir != -1:
-        next_dir = -1
-        next_dir_time = OS.get_ticks_msec() + react_time
-    elif Player.position.x > position.x and next_dir != 1:
-        next_dir = 1
-        next_dir_time = OS.get_ticks_msec() + react_time
-    elif Player.position.x == position.x and next_dir != 0:
-        next_dir = 0
-        next_dir_time = OS.get_ticks_msec() + react_time
+	if Player.position.x < position.x and next_dir != -1:
+		next_dir = -1
+		next_dir_time = OS.get_ticks_msec() + react_time
+	elif Player.position.x > position.x and next_dir != 1:
+		next_dir = 1
+		next_dir_time = OS.get_ticks_msec() + react_time
+	elif Player.position.x == position.x and next_dir != 0:
+		next_dir = 0
+		next_dir_time = OS.get_ticks_msec() + react_time
 
-    if OS.get_ticks_msec() > next_dir_time:
-        dir = next_dir
+	if OS.get_ticks_msec() > next_dir_time:
+		dir = next_dir
 
-    #gravity i guess
-    if(!is_on_floor()):
-        vel.y += 7.5
+	#gravity i guess
+	if(!is_on_floor()):
+		vel.y += 7.5
 
-    vel.x = dir * 100
+	vel.x = dir * 100
 
-    vel = move_and_slide(vel, up_direction, false, 4)
+	vel = move_and_slide(vel, up_direction, false, 4)
 
-    
-    
+	
+	
 
 	
 
